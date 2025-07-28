4f1a28716b090812237da799f5653d07fa3f1563
Dipanshu Rawat <dipanshurawat4@gmail.com>
2025-07-29 04:53:22 +0530

    Dashboard rough completed

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,7 +1,22 @@
 export default function History() {
   return (
-    <div>
-      
+    <div className="card history-card">
+      <h4>History</h4>
+      <p>
+        You have no attempts! Press <b>Start</b> to begin ⭐️
+      </p>
+      <div className="history-list">
+        <div className="card-button-secondary">
+          <div>
+              <p>Started</p>
+              <h6>Jul 29 2025</h6>
+          </div>
+          <div>
+            <p>Streak</p>
+            <h6>53</h6>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
