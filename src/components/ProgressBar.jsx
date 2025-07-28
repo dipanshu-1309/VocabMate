a88dfd876019066b7cae627c12b18ec0e98f4edc
Dipanshu Rawat <dipanshurawat4@gmail.com>
2025-07-29 03:13:43 +0530

    working on progress bar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,7 +1,22 @@
 export default function ProgressBar() {
+
+  const text = "Hello World!";
+  const arr = [0,1,2,3,4,5,6,7,8,9]
   return (
-    <div>
-      
+    <div className="level">
+      <div>
+        <h4>{text}</h4>
+      </div>
+
+      {arr.map((element, elementIdx) => {
+        return (
+          <div className="level-bar" key={elementIdx}>{element}</div>
+        )
+      })}
+
+      <div className="xp" style={{width: `${40}%`}}>
+        
+      </div>
     </div>
   )
 }
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,22 +1,32 @@
-export default function Stats() {
+import ProgressBar from "./ProgressBar";
 
-  const name = 'Dipanshu'
-  const day = 16
+export default function Stats() {
+  const name = "Dipanshu";
+  const day = 16;
 
   return (
     <div className="card stats-card">
       <div className="welcome-text">
-        <h1>Welcome</h1>
-        <h4 className="text-large">
-          {name}
-        </h4>
-      </div>
-      
-      <div className="stats-column">
-        <p>Streak 🔥</p>
-        <h4>{day - 1}</h4>
+        <h6>Welcome</h6>
+        <h4 className="text-large">{name}</h4>
       </div>
 
+      <div className="stats-column">
+        <div>
+          <p>Streak 🔥</p>
+          <h4>{day - 1}</h4>
+        </div>
+        <div>
+          <p>Words seen</p>
+          <h4>{101}</h4>
+        </div>
+        <div>
+          <p>Accuracy</p>
+          <h4>{(93.3).toFixed(2)}</h4>
+        </div>
+      </div>
+
+      <ProgressBar />
     </div>
-  )
+  );
 }
